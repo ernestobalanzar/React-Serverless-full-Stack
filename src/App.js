46b4c8d7eb998c8a0b068810627325cc98dce0ef
Ernesto <ernesto.bs7@gmail.com>
2022-01-03 13:28:38 -0500

    Styles components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Game from "./pages/Game";
+import HighScores from "./pages/HighScores";
+import GameOver from "./pages/GameOver";
+import Home from "./pages/Home";
+import NavBar from "./components/NavBar";
+import { Container } from "./styled/Container";
+import { Main } from "./styled/Main";
+import Global from "./styled/Global";
 
 function App() {
-  return <div className="App">APP</div>;
+  return (
+    <Router>
+      <Global />
+      <Main>
+        <Container>
+          <NavBar />
+          <Routes>
+            <Route exact path="/game" element={<Game />} />
+            <Route exact path="/highScores" element={<HighScores />} />
+            <Route exact path="/gameOver" element={<GameOver />} />
+            <Route exact path="/" element={<Home />} />
+          </Routes>
+        </Container>
+      </Main>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/styled/Main.js
@@ -0,0 +1,7 @@
+import style from "styled-components";
+
+export const Main = style.div`
+    margin: 0;
+    background-color: #f9f9f9;
+    min-height:100vh;
+`;
