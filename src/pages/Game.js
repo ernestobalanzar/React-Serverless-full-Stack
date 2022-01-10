3961c4d5176c93c881b99d58458248efb94428f0
Ernesto <ernesto.bs7@gmail.com>
2022-01-10 15:55:09 -0500

    Game, game over and navigation

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
+import { useNavigate } from "react-router-dom";
+import { useScore } from "../context/ScoreContext";
 import {
   StyleCharacter,
   StyledGame,
@@ -8,14 +10,90 @@ import {
 } from "../styled/Game";
 
 export default function Game() {
+  const MAX_SECONDS = 5;
+  const characters = "fdfkdsajkweuinvieippqkdnttawer";
+
+  const [score, setScore] = useScore(0);
+  const [currentChar, setCurrentChar] = useState("");
+  const [ms, setMs] = useState(0);
+  const [seconds, setSeconds] = useState(MAX_SECONDS);
+
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    setRandomChar();
+    setScore(0);
+    const currentTime = new Date();
+    const interval = setInterval(() => updateTime(currentTime), 1);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    if (seconds <= -1) {
+      navigate("/gameOver");
+    }
+  }, [seconds, ms, navigate]);
+
+  const handleKeyUp = useCallback(
+    (e) => {
+      console.log(e.key);
+      if (e.key === currentChar) {
+        setScore((prevScore) => prevScore + 1);
+        setRandomChar();
+      } else {
+        if (score > 0) {
+          setScore((prevScore) => prevScore - 1);
+        }
+      }
+      setRandomChar();
+    },
+    [currentChar]
+  );
+
+  useEffect(() => {
+    document.addEventListener("keyup", handleKeyUp);
+    return () => document.removeEventListener("keyup", handleKeyUp);
+  }, [handleKeyUp]);
+
+  const updateTime = (startTime) => {
+    const endTime = new Date();
+    const msPassedStr = (endTime.getTime() - startTime.getTime()).toString();
+    const formattedMSString = ("0000" + msPassedStr).slice(-5);
+    const updatedSeconds =
+      MAX_SECONDS - parseInt(formattedMSString.substring(0, 2));
+    const updatedMs =
+      1000 -
+      parseInt(formattedMSString.substring(formattedMSString.length - 3));
+
+    setSeconds(addLeadingZeros(updatedSeconds, 2));
+    setMs(addLeadingZeros(updatedMs, 3));
+  };
+
+  const addLeadingZeros = (num, lenght) => {
+    let zeros = "";
+    for (let i = 0; i < lenght; i++) {
+      zeros += "0";
+    }
+    return (zeros + num).slice(-lenght);
+  };
+
+  const setRandomChar = () => {
+    const randomInt = Math.floor(Math.random() * characters.length);
+    setCurrentChar(characters[randomInt]);
+  };
+
   return (
     <StyledGame>
       <StyleScore>
-        Score:<Strong>0</Strong>
+        Score:<Strong>{score}</Strong>
       </StyleScore>
-      <StyleCharacter>A</StyleCharacter>
+      <StyleCharacter>{currentChar}</StyleCharacter>
       <StyleTimer>
-        Time: <Strong>00:000</Strong>
+        Time:{" "}
+        <Strong>
+          {seconds}:{ms}
+        </Strong>
       </StyleTimer>
     </StyledGame>
   );
--- src/pages/GameOver.js
+++ src/pages/GameOver.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { useScore } from "../context/ScoreContext";
+import { useNavigate } from "react-router-dom";
+import { StyleLinkButton } from "../styled/NavBar";
 
 export default function GameOver() {
+  const [score] = useScore();
+  let navigate = useNavigate();
+
+  if (score === -1) {
+    navigate("/");
+  }
   return (
     <div>
       <h1>Game Over</h1>
+      <p>{score}</p>
+      <StyleLinkButton to="/">Home</StyleLinkButton>
+      <StyleLinkButton to="/game">Play Again</StyleLinkButton>
     </div>
   );
 }
